b3880e6aeb8e0194dd3e04cb955c646d22751ee9
DoctorBazooka <18335242+DoctorBazooka@users.noreply.github.com>
2020-12-01 22:17:57 +0000

    Make smaller!

--- day1.py
+++ day1.py
@@ -1,15 +1,11 @@
 def sum_to(target):
-    vals = set()
     with open("day1_input.txt") as f:
-        for line in f:
-            if target - (x := int(line)) in vals:
-                return x * (target-x)
-            vals.add(x)
-
+        vals = set()
+        for a in map(int, f):
+            if target - a in vals:
+                return a * (target - a)
+            vals.add(a)
 with open("day1_input.txt") as f:
-    vals = set()
     for a in map(int, f):
         if b := sum_to(2020 - a):
             print(a * b)
-            break
-        vals.add(a)
