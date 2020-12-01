09e70ef7cd65c1b5585de52dd40b04572a51e1f8
DoctorBazooka <18335242+DoctorBazooka@users.noreply.github.com>
2020-12-01 21:57:27 +0000

    Refactor

--- day1.py
+++ day1.py
@@ -1,7 +1,9 @@
-with open("day1_input.txt") as f:
+def sum_to(nums, target):
     vals = set()
-    for line in f:
-        if 2020 - (x := int(line)) in vals:
-            print(x * (2020-x))
-            break
+    for line in nums:
+        if target - (x := int(line)) in vals:
+            return x * (target-x)
         vals.add(x)
+
+with open("day1_input.txt") as f:
+    print(sum_to(f, 2020))
