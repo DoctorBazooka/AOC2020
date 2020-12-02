d06da0449c2a66028e926b208af25a5d1a939194
Matthew Cummins <mcummins16@bloomberg.net>
2020-12-02 10:17:53 +0000

    Solve day 2 part 2

--- day2.py
+++ day2.py
@@ -1,13 +1,23 @@
 from collections import Counter
 
 def parse_line(line):
-    policy, letter, data = line.split(" ")
-    return [*tuple(int(x) for x in policy.split("-")), letter[0], Counter(data)]
+    limits, letter, password = line.split(" ")
+    return [*tuple(int(x) for x in limits.split("-")), letter[0], password]
 
-count = 0
+def part1_policy(x, y, letter, password):
+    return letter in password and x <= Counter(password)[letter] <= y
+
+def part2_policy(x, y, letter, password):
+    return (password[x-1] == letter) ^ (password[y-1] == letter)
+
+count_part1 = 0
+count_part2 = 0
 with open("day2_input.txt") as f:
     for line in f:
-        low, high, letter, data = parse_line(line)
-        if letter in data and low <= data[letter] <= high:
-            count += 1
-print(count)
+        x, y, letter, password = parse_line(line)
+        if part1_policy(x, y, letter, password):
+            count_part1 += 1
+        if part2_policy(x, y, letter, password):
+            count_part2 += 1
+print(count_part1)
+print(count_part2)
