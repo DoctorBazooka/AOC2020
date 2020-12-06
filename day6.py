c55fd8422c751597668a44a3cbeb637fa071572b
DoctorBazooka <18335242+DoctorBazooka@users.noreply.github.com>
2020-12-06 05:23:36 +0000

    Solve day6

--- day6.py
+++ day6.py
@@ -1,5 +1,20 @@
+def parse(f):
+    current_party = []
+    for line in f:
+        if len(line.strip()) == 0:
+            yield current_party
+            current_party = []
+        else:
+            current_party.append(set(line.strip()))
+    yield current_party
 
+count_part1 = 0
+count_part2 = 0
 
 with open("day6_input.txt") as f:
-    for line in f:
-        pass
+    for party in parse(f):
+       count_part1 += len(set.union(*party))
+       count_part2 += len(set.intersection(*party))
+
+print(count_part1)
+print(count_part2)
