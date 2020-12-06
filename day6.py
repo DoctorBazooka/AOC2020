23000d1822d613292bdab8973b375f44253a31de
DoctorBazooka <18335242+DoctorBazooka@users.noreply.github.com>
2020-12-06 13:26:28 +0000

    Simplfy day6

--- day6.py
+++ day6.py
@@ -1,14 +1,11 @@
-def parse(f):
-    for party in f.read().split("\n\n"):
-        yield [set(x) for x in party.split("\n")]
-
 count_part1 = 0
 count_part2 = 0
 
 with open("day6_input.txt") as f:
-    for party in parse(f):
-       count_part1 += len(set.union(*party))
-       count_part2 += len(set.intersection(*party))
+    for data in f.read().split("\n\n"):
+        party = [set(x) for x in data.split("\n")]
+        count_part1 += len(set.union(*party))
+        count_part2 += len(set.intersection(*party))
 
 print(count_part1)
 print(count_part2)
