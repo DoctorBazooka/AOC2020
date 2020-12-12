99152e5b716be3edaef160b0a8e1e87cbc59bf22
DoctorBazooka <18335242+DoctorBazooka@users.noreply.github.com>
2020-12-12 01:47:41 +0000

    SImplify day11, it's still a mess

--- day11.py
+++ day11.py
@@ -11,78 +11,47 @@ def neighbour_count_part1(old, row, col):
                 count += 1
     return count
 
-def get_cell_part1(old, row, col):
-    if old[row][col] == ".":
-        return "."
-    elif old[row][col] == "L":
-        return "#" if neighbour_count_part1(old, row, col) == 0 else "L"
-    else:
-        return "L" if neighbour_count_part1(old, row, col) >= 4 else "#"
-
-def get_next_board_part1(old):
-    new = []
-    for row in range(len(old)):
-        new.append([])
-        for col in range(len(old[0])):
-            new[row].append(get_cell_part1(old, row, col))
-    return new
-
-def part1(initial_board):
-    prev_board = []
-    curr_board = initial_board
-    while curr_board != prev_board:
-        prev_board = curr_board
-        curr_board = get_next_board_part1(curr_board)
-
-    count = 0
-    for row in curr_board:
-        for seat in row:
-            if seat == "#":
-                count += 1
-    return count
-
-
-def raycast(old, row, col, dx, dy):
-    row += dx
-    col += dy
-    while 0 <= row < len(old) and 0 <= col < len(old[0]):
-        if old[row][col] in {"L", "#"}:
-            return 1 if old[row][col] == "#" else 0
+def neighbour_count_part2(old, row, col):
+    
+    def raycast(old, row, col, dx, dy):
         row += dx
         col += dy
-    return 0
+        while 0 <= row < len(old) and 0 <= col < len(old[0]):
+            if old[row][col] in {"L", "#"}:
+                return 1 if old[row][col] == "#" else 0
+            row += dx
+            col += dy
+        return 0
 
-def neighbour_count_part2(old, row, col):
     count = 0
     directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
     for dx, dy in directions:
         count += raycast(old, row, col, dx, dy)
     return count
 
-def get_cell_part2(old, row, col):
+def get_cell(old, row, col, count_neighbours, tolerance):
     if old[row][col] == ".":
         return "."
     elif old[row][col] == "L":
-        return "#" if neighbour_count_part2(old, row, col) == 0 else "L"
+        return "#" if count_neighbours(old, row, col) == 0 else "L"
     else:
-        return "L" if neighbour_count_part2(old, row, col) >= 5 else "#"
+        return "L" if count_neighbours(old, row, col) >= tolerance else "#"
 
-def get_next_board_part2(old):
+def get_next_board(old, count_neighbours, tolerance):
     new = []
     for row in range(len(old)):
         new.append([])
         for col in range(len(old[0])):
-            new[row].append(get_cell_part2(old, row, col))
+            new[row].append(get_cell(old, row, col, count_neighbours, tolerance))
     return new
-    
 
-def part2(initial_board):
+def run(initial_board, count_neighbours, tolerance):
     count = 0
     prev_board = []
     curr_board = initial_board
     while curr_board != prev_board:
         prev_board = curr_board
-        curr_board = get_next_board_part2(curr_board)
+        curr_board = get_next_board(curr_board, count_neighbours, tolerance)
         count += 1
 
     count = 0
@@ -93,6 +62,5 @@ def part2(initial_board):
     return count
 
 initial_board = load_board()
-
-print(part1(initial_board))
-print(part2(initial_board))
+print(run(initial_board, neighbour_count_part1, 4))
+print(run(initial_board, neighbour_count_part2, 5))
