9ba5b60b51ef4abff52bac68961e08a802c7f3f6
DoctorBazooka <18335242+DoctorBazooka@users.noreply.github.com>
2020-12-12 14:09:36 +0000

    Cleanup

--- day12.py
+++ day12.py
@@ -5,17 +5,20 @@ def instructions():
 
 def move(pos, direction, value):
     x, y = pos
-    if direction == "N":
-        return (x, y + value)
-    if direction == "S":
-        return (x, y - value)
-    if direction == "E":
-        return (x + value, y)
-    if direction == "W":
-        return (x - value, y)
+    return {
+        "N": (x, y + value),
+        "S": (x, y - value),
+        "E": (x + value, y),
+        "W": (x - value, y)
+    }[direction]
 
 def get_direction(rotation):
-    return {0: "N", 90: "E", 180: "S", 270: "W"}[rotation]
+    return {
+        0: "N",
+        90: "E",
+        180: "S",
+        270: "W"
+    }[rotation]
 
 def part1():
     position = (0, 0)
@@ -32,4 +35,34 @@ def part1():
             position = move(position, direction, value)
     return abs(position[0]) + abs(position[1])
 
+
+def rotate_waypoint(waypoint, rotation): # Rotate clockwise
+    x, y = waypoint
+    return {
+        0: (x, y),
+        90: (y, -x),
+        180: (-x, -y),
+        270: (-y, x)
+    }[rotation]
+
+def move_to_waypoint(position, waypoint, times):
+    x, y = position
+    dx, dy = waypoint
+    return (x + times * dx, y + times * dy)
+
+def part2():
+    position = (0, 0)
+    waypoint = (10, 1) # Offset of waypoint in world space coords
+    for action, value in instructions():
+        if action in {"N", "E", "S", "W"}:
+            waypoint = move(waypoint, action, value)
+        elif action == "R":
+            waypoint = rotate_waypoint(waypoint, value)
+        elif action == "L":
+            waypoint = rotate_waypoint(waypoint, (-value % 360))
+        elif action == "F":
+            position = move_to_waypoint(position, waypoint, value)
+    return abs(position[0]) + abs(position[1])
+
 print(part1())
+print(part2())
